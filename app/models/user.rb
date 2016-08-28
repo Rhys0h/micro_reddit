bd48cba8d5c1706578b14655088df99d1a864d71
Rhys <rphillips1@live.com.au>
2016-08-28 18:02:17 +0700

    Complete micro-reddit

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+	has_many :posts
+	has_many :comments
+	validates :name, presence: true, 
+	                 length: { maximum: 20 }, 
+	                 uniqueness: { case_sensitive: false }
+end
